4a16d3d61b47eb3693b0bfa08429931970ce6c86
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2018-02-17 18:48:59 +0300

    Add files via upload

--- call_program.py
+++ call_program.py
@@ -1,9 +1,12 @@
 import subprocess
+import os
 
-process=subprocess.run("convert Source\\47c8937f8ae4966e356219a9015b1c67.jpg -resize 200 Result\\image1.jpg")
-process=subprocess.run("convert Source\\26694fe81544dcf4c14217d6ace7e4f6.jpg -resize 200 Result\\image2.jpg")
-process=subprocess.run("convert Source\\face-04.jpg -resize 200 Result\\image3.jpg")
-process=subprocess.run("convert Source\\men-beard-styles-for-round-face.jpg -resize 200 Result\\image4.jpg")
-process=subprocess.run("convert Source\\trump_mug.jpg -resize 200 Result\\image5.jpg")
+folder = os.listdir('Source')
+if not os.path.exists("Result"):
+    os.mkdir("Result")
+for file in folder:
+    if file.endswith(".jpg"):
+        subprocess.run("convert Source\\{0} -resize 200 Result\\new_{0}".format(file))
+
 
 
